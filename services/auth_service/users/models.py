7292d6fc02fa95385af58887d8c40174effc5ce1
Meriodasuzwe <raxataliev05@gmail.com>
2026-01-05 17:53:27 +0500

    Docker setup,custom user model

---
+++ services/auth_service/users/models.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    # Роли для доступа (RBAC)
+    ROLE_CHOICES = (
+        ('admin', 'Администратор'),
+        ('teacher', 'Преподаватель'),
+        ('student', 'Студент'),
+    )
+    
+    role = models.CharField(
+        max_length=10, 
+        choices=ROLE_CHOICES, 
+        default='student',
+        verbose_name="Роль"
+    )
+    
+    # Специфика КЗ
+    iin = models.CharField(
+        max_length=12, 
+        unique=True, 
+        null=True, 
+        blank=True, 
+        verbose_name="ИИН"
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
